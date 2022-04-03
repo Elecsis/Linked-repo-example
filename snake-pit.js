e07919d8a498be22fecc3a500597a96dceb039f8
Lex <owner@Owners-MBP.attlocal.net>
2022-04-03 10:01:38 -0400

    created and set background, canvas with border, linked imgs, css, js & html, created 1st snake square in canvas

---
+++ snake-pit.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('snake-pit-area');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'black';
+ctx.fillRect(260, 260, 24, 24);
+ctx.strokeStyle = "red";
+ctx.strokeRect(260, 260, 24, 24);
+
